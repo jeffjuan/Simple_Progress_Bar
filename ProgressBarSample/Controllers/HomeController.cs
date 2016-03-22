afdf22772261571e3ea0667f97f688876b403780
Jeff Juan(阮哲夫) <JeffJuan@86shop.com.tw>
2016-03-22 12:12:40 +0800

    progress bar

---
+++ ProgressBarSample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgressBarSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public bool Hello()
+        {
+            for(int i =0;i<10000;i++)
+            {
+                for (int j = 0; j < 100000; j++)
+                {
+
+                }
+            }
+            return true;
+        }
+    }
+}
